85228e894bb934ec8f6a4df2fecc038047054723
amusukwa <amusukwa@gmail.com>
2023-05-29 07:49:24 -0700

    handle_integer

--- main.h
+++ main.h
@@ -3,4 +3,8 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 void hande_conversion(const char * const format, ...);
+void bin(unsigned int num);
+int toint(char str[]);
+void tostring(char str[], int num);
+int print_integer(const char *formatstr, ...);
 #endif
