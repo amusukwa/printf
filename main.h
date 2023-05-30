92993a50d8c173d180a71f0aeec6bd08fe4feaf0
amusukwa <amusukwa@gmail.com>
2023-05-30 23:24:11 +0200

    adding print_integer

--- main.h
+++ main.h
@@ -1,10 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 void hande_conversion(const char * const format, ...);
 void bin(unsigned int num);
-int toint(char str[]);
-void tostring(char str[], int num);
-int print_integer(const char *formatstr, ...);
+int print_integer(int arg);
+void tu_u(unsigned int n);
+int to_hexa( unsigned int num);
+void to_octal(long dec);
 #endif
