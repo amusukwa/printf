62fba786888884f86659d3acc334e1e05f5d1a8d
amusukwa <amusukwa@gmail.com>
2023-06-03 18:29:50 +0200

    print_binary

--- testdir2/handle_binary.c
+++ testdir2/handle_binary.c
@@ -4,46 +4,42 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * print_int - prints a binary number
+ * print_binary - prints a binary number
  * @number: input number
  * Return: count of charaters
  */
 int print_binary(int number)
 {
-    int i, n, bit;
+int i, n, bit;
 
-    n = 0;
+n = 0;
 
-    if (number < 0)
-    {
-        _putchar('-');
-        n++;
-        number = -number;
-    }
+if (number < 0)
+{
+_putchar('-');
+n++;
+number = -number;
+}
 
-    if (number == 0)
-    {
-        _putchar('0');
-        n++;
-        return n;
-    }
+if (number == 0)
+{
+_putchar('0');
+n++;
+return (n);
+}
 
-    
-    for (i = 31; i >= 0; i--)
-    {
-        if ((number >> i) & 1)
-            break;
-    }
-
-    
-    for (; i >= 0; i--)
-    {
-        bit = (number >> i) & 1;
-        _putchar('0' + bit);
-        n++;
-    }
-
-    return n;
+for (i = 31; i >= 0; i--)
+{
+if ((number >> i) & 1)
+break;
+}
+for (; i >= 0; i--)
+{
+bit = (number >> i) & 1;
+_putchar('0' + bit);
+n++;
+}
+return (n);
 }
 
 
--- testdir2/handle_integer.c
+++ testdir2/handle_integer.c
@@ -8,29 +8,28 @@
  */
 int print_int(int number)
 {
-        int i, n, d;
+int i, n, d;
 
-	n = 0;
+n = 0;
 
-        if (number < 0)
-        {
-                _putchar('-');
-                n++;
-                number = -number;
-        }
-        i = 1;
-
-        while ((number / i) > 9)
-        {
-                i *= 10;
-        }
-        while (i > 0)
-        {
-                d = number / i;
-                _putchar ('0' + d);
-                n++;
-                number %= i;
-                i /= 10;
-        }
-	return (n);
+if (number < 0)
+{
+_putchar('-');
+n++;
+number = -number;
+}
+i = 1;
+while ((number / i) > 9)
+{
+i *= 10;
+}
+while (i > 0)
+{
+d = number / i;
+_putchar ('0' + d);
+n++;
+number %= i;
+i /= 10;
+}
+return (n);
 }
